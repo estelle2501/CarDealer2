6c9895b821060865a540206e399a7e72af230178
Magda <estelle2501@gmail.com>
2017-11-15 19:25:21 +0100

    Added getCars method to CarController

--- src/main/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarController.java
+++ src/main/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarController.java
@@ -1,5 +1,7 @@
 package com.wordpress.zapiskiprogramistki.CarDealer2.car;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,17 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
-	
+
 	@Autowired
 	private CarFacade carFacade;
-	
-	 public CarController(CarFacade carFacade) {
+
+	public CarController(CarFacade carFacade) {
 		this.carFacade = carFacade;
 	}
-	
+
 	@GetMapping("cars/{id}")
-	CarDto getCar(@PathVariable int id){
+	CarDto getCar(@PathVariable int id) {
 		return carFacade.findById(id);
 	}
-	
+
+	@GetMapping("cars")
+	List<CarDto> getCars() {
+		return carFacade.findAll();
+	}
+
 }
