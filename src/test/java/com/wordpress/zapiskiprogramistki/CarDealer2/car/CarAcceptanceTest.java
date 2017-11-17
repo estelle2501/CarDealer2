10814fe7c5e9a819f8da3893e8646c673084d933
Magda <estelle2501@gmail.com>
2017-11-17 20:19:31 +0100

    Test get("/cars") for two cars

--- src/test/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarAcceptanceTest.java
+++ src/test/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.wordpress.zapiskiprogramistki.CarDealer2.car;
 
-import javax.swing.text.AbstractDocument.Content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,13 +14,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
 
 import com.wordpress.zapiskiprogramistki.CarDealer2.ApplicationConfig;
 
@@ -30,6 +28,7 @@ public class CarAcceptanceTest {
 
 	private String carBrandAlfa = "Alfa Romeo";
 	private String carBrandToyota = "Toyota";
+	private String carBrandFiat = "Fiat";
 
 	@InjectMocks
 	CarController controller;
@@ -47,24 +46,56 @@ public class CarAcceptanceTest {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
+//Why shouldGetTwoCars runs before shouldGetOneCar regardless the code sequence?
+	
 	@Test
-	public void shouldGetCar() throws Exception {
+	public void shouldGetOneCar() throws Exception {
 
 		CarDto carDtoAlfa = createCarDto(carBrandAlfa);
 
 		carDtoAlfa = carFacade.add(carDtoAlfa);
-		
+
 		mockMvc.perform(
 				MockMvcRequestBuilders.get("/cars").contentType(
 						MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(
 						content().string(
-								"[{\"id\":0,\"brand\":\"Alfa Romeo\"}]"));
+								"[{\"id\":2,\"brand\":\"Alfa Romeo\"}]"));
+		/*
+		 * Why id=2 not id=0 for Alfa, as it is the only car in DB now? Even if
+		 * Fiat and Toyota were deleted, their id's=0,1 were already used and
+		 * cannot be "reused" for creating another car during the same test
+		 * session
+		 */
+		carFacade.delete(carDtoAlfa.getId());
+	}
+	
+	@Test
+	public void shouldGetTwoCars() throws Exception {
+
+		CarDto carDtoFiat = createCarDto(carBrandFiat);
+		CarDto carDtoToyota = createCarDto(carBrandToyota);
+
+		carDtoFiat = carFacade.add(carDtoFiat);
+		carDtoToyota = carFacade.add(carDtoToyota);
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.get("/cars").contentType(
+						MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(
+						content()
+								.string("[{\"id\":0,\"brand\":\"Fiat\"},{\"id\":1,\"brand\":\"Toyota\"}]"));
+
+		carFacade.delete(carDtoFiat.getId());
+		carFacade.delete(carDtoToyota.getId());
 	}
 
 	static private CarDto createCarDto(String carBrand) {
 
 		return CarDto.builder().brand(carBrand).build();
 	}
+	
+	
 }
