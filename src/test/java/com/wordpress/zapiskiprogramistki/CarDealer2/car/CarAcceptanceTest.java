f03dda3ac7ca39e2ad19cd658df1ff853444b479
Magda <estelle2501@gmail.com>
2017-11-19 18:44:29 +0100

    Refactoring CarAcceptanceTest

--- src/test/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarAcceptanceTest.java
+++ src/test/java/com/wordpress/zapiskiprogramistki/CarDealer2/car/CarAcceptanceTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
 import com.wordpress.zapiskiprogramistki.CarDealer2.ApplicationConfig;
 
 @RunWith(SpringRunner.class)
@@ -46,47 +45,36 @@ public class CarAcceptanceTest {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
-//Why shouldGetTwoCars runs before shouldGetOneCar regardless the code sequence?
-	
 	@Test
-	public void shouldGetOneCar() throws Exception {
+	public void testCarController() throws Exception {
 
 		CarDto carDtoAlfa = createCarDto(carBrandAlfa);
 
 		carDtoAlfa = carFacade.add(carDtoAlfa);
-
+//Test with one car in DB
 		mockMvc.perform(
 				MockMvcRequestBuilders.get("/cars").contentType(
 						MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(
 						content().string(
-								"[{\"id\":2,\"brand\":\"Alfa Romeo\"}]"));
-		/*
-		 * Why id=2 not id=0 for Alfa, as it is the only car in DB now? Even if
-		 * Fiat and Toyota were deleted, their id's=0,1 were already used and
-		 * cannot be "reused" for creating another car during the same test
-		 * session
-		 */
+								"[{\"id\":0,\"brand\":\"Alfa Romeo\"}]"));
+
 		carFacade.delete(carDtoAlfa.getId());
-	}
-	
-	@Test
-	public void shouldGetTwoCars() throws Exception {
 
 		CarDto carDtoFiat = createCarDto(carBrandFiat);
 		CarDto carDtoToyota = createCarDto(carBrandToyota);
 
 		carDtoFiat = carFacade.add(carDtoFiat);
 		carDtoToyota = carFacade.add(carDtoToyota);
-
+//Test with two cars in DB
 		mockMvc.perform(
 				MockMvcRequestBuilders.get("/cars").contentType(
 						MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(
 						content()
-								.string("[{\"id\":0,\"brand\":\"Fiat\"},{\"id\":1,\"brand\":\"Toyota\"}]"));
+								.string("[{\"id\":1,\"brand\":\"Fiat\"},{\"id\":2,\"brand\":\"Toyota\"}]"));
 
 		carFacade.delete(carDtoFiat.getId());
 		carFacade.delete(carDtoToyota.getId());
@@ -96,6 +84,5 @@ public class CarAcceptanceTest {
 
 		return CarDto.builder().brand(carBrand).build();
 	}
-	
-	
+
 }
